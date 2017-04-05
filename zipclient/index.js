6e3cd454c688777a2fd21e2d7d20539a5db71b50
Duncan Andrew <duncanandrew@live.com>
2017-04-05 15:14:45 -0700

    in-class work

---
+++ zipclient/index.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    var form = document.getElementById("the-form");
+    var city = document.getElementById("city");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        fetch("http://138.68.253.36/zips/city/" + city.value)
+            .then(function(response){
+                console.log(response);
+                return response.json();
+            })
+            .then(function(data){
+                var output = document.createElement("p");
+                console.log(data);
+                output.textContent = JSON.stringify(data);
+                document.body.appendChild(output);
+            })
+            .catch(function(err){
+                console.error(err);
+            })
+
+    })
+}
