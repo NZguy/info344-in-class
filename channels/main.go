b1d1b1f9080d08f7facd146e0449b4782eb3acf5
Duncan Andrew <duncanandrew@live.com>
2017-05-03 14:17:32 -0700

    in class base work

--- channels/main.go
+++ channels/main.go
@@ -1,10 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 //someLongFunc is a function that might
 //take a while to complete, so we want
 //to run it on its own go routine
-func someLongFunc() {
-
+func someLongFunc(ch chan int) {
+	r := rand.Intn(2000)
+	d := time.Duration(r)
+	time.Sleep(time.Millisecond * d)
+	ch <- r // Writing from the channel
 }
 
 func main() {
@@ -14,4 +23,17 @@ func main() {
 	//passing the channel so that
 	//someLongFunc() can communicate
 	//its results
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("starting long-running func...")
+	n := 10
+	ch := make(chan int, n)
+	start := time.Now()
+	for i := 0; i < n; i++ {
+		go someLongFunc(ch) // go means run concurrently
+	}
+	for i := 0; i < n; i++ {
+		result := <-ch // Reading from the channel, this code yeilds when until channel is written to
+		fmt.Printf("result was %d\n", result)
+	}
+	fmt.Printf("took %v\n", time.Since(start))
 }
--- concur/main.go
+++ concur/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"crypto/sha256"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path"
 	"time"
 )
 
@@ -15,16 +20,46 @@ func processFile(filePath string, ch chan int) {
 	//TODO: open the file, scan each line,
 	//do something with the word, and write
 	//the results to the channel
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(f)
+	n := 0
+	for scanner.Scan() {
+		n++
+
+		for i := 0; i < 100; i++ {
+			h := sha256.New()
+			h.Write(scanner.Bytes())
+			_ = h.Sum(nil)
+		}
+	}
+	f.Close()
+	ch <- n
 }
 
 func processDir(dirPath string) {
 	//TODO: iterate over the files in the directory
 	//and process each, first in a serial manner,
 	//and then in a concurrent manner
+	fileinfos, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ch := make(chan int, len(fileinfos))
+	for _, fi := range fileinfos {
+		go processFile(path.Join(dirPath, fi.Name()), ch) // Compare concurrent to serial
+	}
+	nWords := 0
+	for i := 0; i < len(fileinfos); i++ {
+		nWords += <-ch
+	}
+	fmt.Printf("processed %d words\n", nWords)
 }
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		fmt.Println(usage)
 		os.Exit(1)
 	}
